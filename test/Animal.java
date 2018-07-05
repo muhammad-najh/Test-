0086439a5e26371b4cc712ea4d1ab1911eb7b45c
ibrahim-zebary <40140035+ibrahim-zebary@users.noreply.github.com>
2018-07-05 15:49:36 +0300

    Add files via upload

---
+++ test/Animal.java
@@ -0,0 +1,32 @@
+package com.example.ibrahim.test;
+
+import android.util.Log;
+
+public class Animal {
+    private String name;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    void doSomething() {
+        Log.i("Action","roar");
+    }
+
+    void doSomething(String food) {
+        Log.i("Action","relax");
+    }
+}
---
+++ test/Lion.java
@@ -0,0 +1,11 @@
+package com.example.ibrahim.test;
+
+import android.util.Log;
+
+public class Lion extends Animal {
+
+    @Override
+    void doSomething() {
+        Log.i("Action","RRRRRoaRRRR");
+    }
+}
